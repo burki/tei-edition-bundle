443d61af629e85139ce28066ccf2f7bd2eb66148
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2024-12-13 16:50:31 +0100

    Switch to explicit nullable types

--- Entity/Place.php
+++ Entity/Place.php
@@ -205,7 +205,7 @@ extends PlaceBase
         return 8;
     }
 
-    public function setParent(Place $parent = null)
+    public function setParent(?Place $parent = null)
     {
         $this->parent = $parent;
     }
