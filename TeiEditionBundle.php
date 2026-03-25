85526d4213ba441fe4883557c8dc35f8d969ca78
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2026-03-25 21:36:14 +0100

    Move to recommended directory structure (PSR-4) Move templates and translations up from Resources to root

--- TeiEditionBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// src/TeiEditionBundle.php
-
-namespace TeiEditionBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TeiEditionBundle extends Bundle {}
---
+++ src/TeiEditionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+// src/TeiEditionBundle.php
+
+namespace TeiEditionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class TeiEditionBundle extends AbstractBundle {}
