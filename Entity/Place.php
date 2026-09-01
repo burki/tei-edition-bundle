f3fd96ed330f214f7097170b762fc0941ce88c57
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2026-09-01 09:57:24 +0200

    Add additional place type

--- Entity/Place.php
+++ Entity/Place.php
@@ -78,6 +78,7 @@ extends PlaceBase
         'states (political divisions)' => 'state',  // http://vocab.getty.edu/aat/300000776
         'departments (political divisions)' => 'department',  // http://vocab.getty.edu/aat/300000772
         'first level subdivisions (political entities)' => 'province',  // http://vocab.getty.edu/aat/300387064
+        'union territories' => 'union territory',    // http://vocab.getty.edu/aat/300387122
         'third level subdivisions (political entities)' => 'district',  // http://vocab.getty.edu/aat/300387064
         'cantons (administrative bodies)' => 'canton', // http://vocab.getty.edu/aat/300000769
         'prefectures' => 'prefecture',              // http://vocab.getty.edu/aat/300235099
